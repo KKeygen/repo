36c44265eb4b9f98781679dc53615e6d868a6e5a
Keygen <2023302111029@whu.edu.cn>
2026-05-02 11:21:49 +0800

    实现公共工具类与基础常量定义

---
+++ dismai-common/src/main/java/com/dismai/config/DismaiCommonAutoConfig.java
@@ -0,0 +1,12 @@
+﻿package com.dismai.config;
+
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+
+public class DismaiCommonAutoConfig {
+    
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jacksonCustom(){
+        return new JacksonCustom();
+    }
+}
---
+++ dismai-common/src/main/java/com/dismai/exception/DismaiFrameException.java
@@ -0,0 +1,61 @@
+﻿package com.dismai.exception;
+
+import com.dismai.common.ApiResponse;
+import com.dismai.enums.BaseCode;
+import lombok.Data;
+
+@Data
+public class DismaiFrameException extends BaseException {
+
+	private Integer code;
+	
+	private String message;
+
+	public DismaiFrameException() {
+		super();
+	}
+
+	public DismaiFrameException(String message) {
+		super(message);
+	}
+	
+	
+	public DismaiFrameException(String code, String message) {
+		super(message);
+		this.code = Integer.parseInt(code);
+		this.message = message;
+	}
+	
+	public DismaiFrameException(Integer code, String message) {
+		super(message);
+		this.code = code;
+		this.message = message;
+	}
+	
+	public DismaiFrameException(BaseCode baseCode) {
+		super(baseCode.getMsg());
+		this.code = baseCode.getCode();
+		this.message = baseCode.getMsg();
+	}
+	
+	public DismaiFrameException(ApiResponse apiResponse) {
+		super(apiResponse.getMessage());
+		this.code = apiResponse.getCode();
+		this.message = apiResponse.getMessage();
+	}
+
+	public DismaiFrameException(Throwable cause) {
+		super(cause);
+	}
+
+	public DismaiFrameException(String message, Throwable cause) {
+		super(message, cause);
+		this.message = message;
+	}
+
+	public DismaiFrameException(Integer code, String message, Throwable cause) {
+		super(message, cause);
+		this.code = code;
+		this.message = message;
+	}
+}
