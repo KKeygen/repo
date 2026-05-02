3c81f2d35d3cccbc2252942bf6c026e497af5f38
KKeygen <2023302111029@whu.edu.cn>
2026-05-02 11:21:49 +0800

    实现公共工具类与基础常量定义

---
+++ dismai-common/src/main/java/com/dismai/core/SpringUtil.java
@@ -0,0 +1,23 @@
+﻿package com.dismai.core;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static com.dismai.constant.Constant.DEFAULT_PREFIX_DISTINCTION_NAME;
+import static com.dismai.constant.Constant.PREFIX_DISTINCTION_NAME;
+
+public class SpringUtil implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    
+    private static ConfigurableApplicationContext configurableApplicationContext;
+    
+    
+    public static String getPrefixDistinctionName(){
+        return configurableApplicationContext.getEnvironment().getProperty(PREFIX_DISTINCTION_NAME,
+                DEFAULT_PREFIX_DISTINCTION_NAME);
+    }
+    
+    @Override
+    public void initialize(final ConfigurableApplicationContext applicationContext) {
+        configurableApplicationContext = applicationContext;
+    }
+}
---
+++ dismai-common/src/main/java/com/dismai/enums/JobRunType.java
@@ -0,0 +1,18 @@
+﻿package com.dismai.enums;
+
+public enum JobRunType {
+    /**
+     * 同步执行
+     * */
+    SYNC_RUN,
+    
+    /**
+     * 异步执行
+     * */
+    ASYNC_RUN;
+    
+    JobRunType() {
+       
+    }
+    
+}
