c23b639844060aedcbe8298c6982c14f6121a6c7
Ea1XonCodeHou <975292748@qq.com>
2026-05-03 14:48:52 +0800

    集成Redis工具辅助类，支持分布式锁与缓存操作

---
+++ dismai-redis-tool-framework/dismai-redis-framework/src/main/java/com/dismai/redis/config/RedisCacheAutoConfig.java
@@ -0,0 +1,14 @@
+﻿package com.dismai.redis.config;
+
+import com.dismai.redis.RedisCacheImpl;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+public class RedisCacheAutoConfig {
+    
+    @Bean
+    public RedisCacheImpl redisCache(@Qualifier("redisToolStringRedisTemplate") StringRedisTemplate stringRedisTemplate){
+        return new RedisCacheImpl(stringRedisTemplate);
+    }
+}
