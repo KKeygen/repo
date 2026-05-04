3b4e8c436e1f92347a63534c2d1c2222d086ddb9
Dipper-study <2023302111286@whu.edu.cn>
2026-05-04 11:29:39 +0800

    Init base data service and its feign client for internal invocation

---
+++ dismai-server-client/dismai-base-data-client/src/main/java/com/dismai/client/BaseDataClientFallback.java
@@ -0,0 +1,36 @@
+﻿package com.dismai.client;
+
+import com.dismai.common.ApiResponse;
+import com.dismai.dto.AreaGetDto;
+import com.dismai.dto.AreaSelectDto;
+import com.dismai.dto.GetChannelDataByCodeDto;
+import com.dismai.enums.BaseCode;
+import com.dismai.vo.AreaVo;
+import com.dismai.vo.GetChannelDataVo;
+import com.dismai.vo.TokenDataVo;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class BaseDataClientFallback implements BaseDataClient{
+    @Override
+    public ApiResponse<GetChannelDataVo> getByCode(final GetChannelDataByCodeDto dto) {
+        return ApiResponse.error(BaseCode.SYSTEM_ERROR);
+    }
+    
+    @Override
+    public ApiResponse<TokenDataVo> get() {
+        return ApiResponse.error(BaseCode.SYSTEM_ERROR);
+    }
+    
+    @Override
+    public ApiResponse<List<AreaVo>> selectByIdList(final AreaSelectDto dto) {
+        return ApiResponse.error(BaseCode.SYSTEM_ERROR);
+    }
+    
+    @Override
+    public ApiResponse<AreaVo> getById(final AreaGetDto dto) {
+        return ApiResponse.error(BaseCode.SYSTEM_ERROR);
+    }
+}
---
+++ dismai-server/dismai-base-data-service/src/main/java/com/dismai/controller/ChannelDataController.java
@@ -0,0 +1,38 @@
+﻿package com.dismai.controller;
+
+import com.dismai.common.ApiResponse;
+import com.dismai.dto.ChannelDataAddDto;
+import com.dismai.dto.GetChannelDataByCodeDto;
+import com.dismai.service.ChannelDataService;
+import com.dismai.vo.GetChannelDataVo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/channel/data")
+@Tag(name = "channel-data", description = "渠道数据")
+public class ChannelDataController {
+    
+    @Autowired
+    private ChannelDataService channelDataService;
+    
+    @Operation(summary = "通过code查询渠道数据")
+    @PostMapping (value = "/getByCode")
+    public ApiResponse<GetChannelDataVo> getByCode(@Valid @RequestBody GetChannelDataByCodeDto getChannelDataByCodeDto) {
+        GetChannelDataVo getChannelDataVo = channelDataService.getByCode(getChannelDataByCodeDto);
+        return ApiResponse.ok(getChannelDataVo);
+    }
+    
+    @Operation(summary = "添加渠道数据")
+    @PostMapping(value = "/add")
+    public ApiResponse<Boolean> add(@Valid @RequestBody ChannelDataAddDto channelDataAddDto) {
+        channelDataService.add(channelDataAddDto);
+        return ApiResponse.ok(true);
+    }
+}
