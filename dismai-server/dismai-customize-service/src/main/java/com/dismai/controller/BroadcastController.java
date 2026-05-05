2b427968f373fe9a3584ccc3d45982d1cd2debd5
Dipper-study <2023302111286@whu.edu.cn>
2026-05-05 14:31:28 +0800

    Implement customize service and high concurrency load balancing rules

---
+++ dismai-server/dismai-customize-service/src/main/java/com/dismai/controller/BroadcastController.java
@@ -0,0 +1,29 @@
+﻿package com.dismai.controller;
+
+import com.dismai.common.ApiResponse;
+import com.dismai.dto.BroadcastCallDto;
+import com.dismai.service.BroadcastService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/broadcast")
+@Tag(name = "broadcast", description = "广播调用")
+public class BroadcastController {
+
+    @Autowired
+    private BroadcastService broadcastService;
+    
+    @Operation(summary  = "广播调用")
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ApiResponse<Void> call(@Valid @RequestBody BroadcastCallDto broadcastCallDto) {
+        broadcastService.call(broadcastCallDto);
+        return ApiResponse.ok();
+    }
+}
