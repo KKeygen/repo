5008d46a31b2101cf29ed94b528f37c371a737ca
KKeygen <2023302111029@whu.edu.cn>
2026-05-05 16:49:53 +0800

    上线并测试验证码服务模块以增强系统防刷能力

---
+++ dismai-captcha-manage-framework/dismai-captcha-framework/src/main/java/com/dismai/config/AjCaptchaStorageAutoConfiguration.java
@@ -0,0 +1,19 @@
+﻿package com.dismai.config;
+
+import com.dismai.properties.AjCaptchaProperties;
+import com.dismai.captcha.service.CaptchaCacheService;
+import com.dismai.captcha.service.impl.CaptchaServiceFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AjCaptchaStorageAutoConfiguration {
+
+    @Bean(name = "AjCaptchaCacheService")
+    @ConditionalOnMissingBean
+    public CaptchaCacheService captchaCacheService(AjCaptchaProperties config){
+        //缓存类型redis/local/....
+        return CaptchaServiceFactory.getCache(config.getCacheType().name());
+    }
+}
---
+++ dismai-captcha-manage-framework/dismai-captcha-framework/src/main/java/com/dismai/config/CaptchaAutoConfig.java
@@ -0,0 +1,30 @@
+﻿package com.dismai.config;
+
+import com.dismai.properties.AjCaptchaProperties;
+import com.dismai.captcha.service.CaptchaCacheService;
+import com.dismai.captcha.service.CaptchaService;
+import com.dismai.captcha.service.impl.CaptchaServiceFactory;
+import com.dismai.service.CaptchaCacheServiceRedisImpl;
+import com.dismai.service.CaptchaHandle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+public class CaptchaAutoConfig {
+    
+    @Bean
+    public CaptchaHandle captchaHandle(CaptchaService captchaService){
+        return new CaptchaHandle(captchaService);
+    }
+    
+    @Bean(name = "AjCaptchaCacheService")
+    @Primary
+    public CaptchaCacheService captchaCacheService(AjCaptchaProperties config, StringRedisTemplate redisTemplate){
+        //缓存类型redis/local/....
+        CaptchaCacheService ret = CaptchaServiceFactory.getCache(config.getCacheType().name());
+        if(ret instanceof CaptchaCacheServiceRedisImpl){
+            ((CaptchaCacheServiceRedisImpl)ret).setStringRedisTemplate(redisTemplate);
+        }
+        return ret;
+    }
+}
