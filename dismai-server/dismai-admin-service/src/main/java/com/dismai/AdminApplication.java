9755769506412130cf869b46ac037db6c5cdf98d
Dipper-study <2023302111286@whu.edu.cn>
2026-05-05 09:47:14 +0800

    Add admin panel basic APIs for backend portal management

---
+++ dismai-server/dismai-admin-service/src/main/java/com/dismai/AdminApplication.java
@@ -0,0 +1,19 @@
+﻿package com.dismai;
+
+import com.dismai.config.DismaiCommonAutoConfig;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableAdminServer
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DismaiCommonAutoConfig.class)
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("nacos.logging.default.config.enabled","false");
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
---
+++ dismai-server/dismai-admin-service/src/main/java/com/dismai/config/MonitorServerConfig.java
@@ -0,0 +1,27 @@
+﻿package com.dismai.config;
+
+import com.dismai.monitor.DingTalkMessage;
+import com.dismai.monitor.MonitorServer;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MonitorServerConfig {
+    
+    @Value("${dingtalk.token:}")
+    private String token;
+    
+    @Bean
+    public DingTalkMessage dingTalkMessage(){
+        return new DingTalkMessage(token);
+    }
+    
+    @Bean
+    public MonitorServer monitorServer(DingTalkMessage dingTalkMessage,InstanceRepository repository){
+        return new MonitorServer(dingTalkMessage,repository);
+    }
+    
+    
+}
