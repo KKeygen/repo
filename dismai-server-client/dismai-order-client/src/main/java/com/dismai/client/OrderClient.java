8cf0ee4316c3105ff0ca305e262237256a48217a
Ea1XonCodeHou <975292748@qq.com>
2026-05-07 11:06:14 +0800

    实现订单创建与状态流转核心业务逻辑

---
+++ dismai-server-client/dismai-order-client/src/main/java/com/dismai/client/OrderClient.java
@@ -0,0 +1,32 @@
+﻿package com.dismai.client;
+
+import com.dismai.common.ApiResponse;
+import com.dismai.dto.AccountOrderCountDto;
+import com.dismai.dto.OrderCreateDto;
+import com.dismai.vo.AccountOrderCountVo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import static com.dismai.constant.Constant.SPRING_INJECT_PREFIX_DISTINCTION_NAME;
+
+@Component
+@FeignClient(value = SPRING_INJECT_PREFIX_DISTINCTION_NAME+"-"+"order-service",fallback = OrderClientFallback.class)
+public interface OrderClient {
+    
+    /**
+     * 创建订单
+     * @param dto 参数
+     * @return 结果
+     * */
+    @PostMapping("/order/create")
+    ApiResponse<String> create(OrderCreateDto dto);
+    
+    /**
+     * 账户下某个节目的订单数量
+     * @param dto 参数
+     * @return 结果
+     * */
+    @PostMapping("/order/account/order/count")
+    ApiResponse<AccountOrderCountVo> accountOrderCount(AccountOrderCountDto dto);
+}
---
+++ dismai-server/dismai-order-service/src/main/java/com/dismai/service/delaysend/DelayOperateProgramDataSend.java
@@ -0,0 +1,29 @@
+﻿package com.dismai.service.delaysend;
+
+import com.dismai.context.DelayQueueContext;
+import com.dismai.core.SpringUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import static com.dismai.service.constant.OrderConstant.DELAY_OPERATE_PROGRAM_DATA_TIME;
+import static com.dismai.service.constant.OrderConstant.DELAY_OPERATE_PROGRAM_DATA_TIME_UNIT;
+import static com.dismai.service.constant.OrderConstant.DELAY_OPERATE_PROGRAM_DATA_TOPIC;
+
+@Slf4j
+@Component
+public class DelayOperateProgramDataSend {
+    
+    @Autowired
+    private DelayQueueContext delayQueueContext;
+    
+    public void sendMessage(String message){
+        try {
+            delayQueueContext.sendMessage(SpringUtil.getPrefixDistinctionName() + "-" + DELAY_OPERATE_PROGRAM_DATA_TOPIC,
+                    message, DELAY_OPERATE_PROGRAM_DATA_TIME, DELAY_OPERATE_PROGRAM_DATA_TIME_UNIT);
+        }catch (Exception e) {
+            log.error("send message error message : {}",message,e);
+        }
+        
+    }
+}
