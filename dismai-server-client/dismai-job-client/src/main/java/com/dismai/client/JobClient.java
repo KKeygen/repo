91c1ddb31d99df3f7a705a63dfb0f8b3deb5c10b
Dipper-study <2023302111286@whu.edu.cn>
2026-05-11 09:33:12 +0800

    Configure standard mybatis-plus plugins and schedule jobs

---
+++ dismai-server-client/dismai-job-client/src/main/java/com/dismai/client/JobClient.java
@@ -0,0 +1,26 @@
+﻿package com.dismai.client;
+
+import com.dismai.common.ApiResponse;
+import com.dismai.dto.JobCallBackDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.validation.Valid;
+
+import static com.dismai.constant.Constant.SPRING_INJECT_PREFIX_DISTINCTION_NAME;
+
+@Component
+@FeignClient(value = SPRING_INJECT_PREFIX_DISTINCTION_NAME+"-"+"job-service",fallback = JobClientFallback.class)
+public interface JobClient {
+    
+    /**
+     * 上报任务状态
+     * @param dto 参数
+     * @return 结果
+     * */
+    @RequestMapping(value = "jobRunRecord/callBack", method = RequestMethod.POST)
+    ApiResponse<Boolean> callBack(@Valid @RequestBody JobCallBackDto dto);
+}
---
+++ dismai-server-client/dismai-job-client/src/main/java/com/dismai/client/JobClientFallback.java
@@ -0,0 +1,15 @@
+﻿package com.dismai.client;
+
+import com.dismai.common.ApiResponse;
+import com.dismai.dto.JobCallBackDto;
+import com.dismai.enums.BaseCode;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobClientFallback implements JobClient {
+    
+    @Override
+    public ApiResponse<Boolean> callBack(final JobCallBackDto dto) {
+        return ApiResponse.error(BaseCode.SYSTEM_ERROR);
+    }
+}
