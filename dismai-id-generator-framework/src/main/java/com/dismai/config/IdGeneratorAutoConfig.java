261c7e23e05599ac4bcd390cc09497c605c082d1
Keygen <2023302111029@whu.edu.cn>
2026-05-16 10:34:14 +0000

    fix(idgen): annotate IdGeneratorAutoConfig with @Configuration to register beans

--- dismai-id-generator-framework/src/main/java/com/dismai/config/IdGeneratorAutoConfig.java
+++ dismai-id-generator-framework/src/main/java/com/dismai/config/IdGeneratorAutoConfig.java
@@ -4,8 +4,10 @@ import com.dismai.toolkit.SnowflakeIdGenerator;
 import com.dismai.toolkit.WorkAndDataCenterIdHandler;
 import com.dismai.toolkit.WorkDataCenterId;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+@Configuration
 public class IdGeneratorAutoConfig {
     
     @Bean
