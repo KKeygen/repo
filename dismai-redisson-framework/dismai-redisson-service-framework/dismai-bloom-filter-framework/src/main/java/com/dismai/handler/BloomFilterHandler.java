eb0bb95b873ad9a1a5ea2ce020bc0139f5d276e9
KKeygen <kkeygen@example.com>
2026-05-24 16:53:42 +0800

    fix: BloomFilterHandler增加add/contains日志追踪，用于监控RBloomFilter实例状态

--- dismai-redisson-framework/dismai-redisson-service-framework/dismai-bloom-filter-framework/src/main/java/com/dismai/handler/BloomFilterHandler.java
+++ dismai-redisson-framework/dismai-redisson-service-framework/dismai-bloom-filter-framework/src/main/java/com/dismai/handler/BloomFilterHandler.java
@@ -3,61 +3,39 @@ package com.dismai.handler;
 import com.dismai.config.BloomFilterProperties;
 import com.dismai.core.SpringUtil;
 import org.redisson.api.RBloomFilter;
-import org.redisson.api.RScript;
 import org.redisson.api.RedissonClient;
-import org.redisson.client.codec.StringCodec;
-import org.redisson.misc.MurmurHash3;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BloomFilterHandler {
     
-    private final RBloomFilter<String> cachePenetrationBloomFilter;
-    private final RedissonClient redissonClient;
-    private final String filterKey;
-    private final RScript script;
+    private static final Logger log = LoggerFactory.getLogger(BloomFilterHandler.class);
     
-    public BloomFilterHandler(RedissonClient redissonClient, BloomFilterProperties bloomFilterProperties) {
-        this.redissonClient = redissonClient;
-        String name = SpringUtil.getPrefixDistinctionName() + "-" + bloomFilterProperties.getName();
-        this.filterKey = name;
-        this.script = redissonClient.getScript(StringCodec.INSTANCE);
-        
-        RBloomFilter<String> filter = redissonClient.getBloomFilter(name);
-        filter.tryInit(bloomFilterProperties.getExpectedInsertions(),
+    private final RBloomFilter<String> cachePenetrationBloomFilter;
+    
+    public BloomFilterHandler(RedissonClient redissonClient, BloomFilterProperties bloomFilterProperties){
+        RBloomFilter<String> filter = redissonClient.getBloomFilter(
+                SpringUtil.getPrefixDistinctionName() + "-" + bloomFilterProperties.getName());
+        filter.tryInit(bloomFilterProperties.getExpectedInsertions(), 
                 bloomFilterProperties.getFalseProbability());
         this.cachePenetrationBloomFilter = filter;
+        log.info("BloomFilterHandler initialized, contains(test)={}", 
+                filter.contains("__init_test__"));
     }
     
     public boolean add(String data) {
-        long size = cachePenetrationBloomFilter.getSize();
-        int hashIterations = cachePenetrationBloomFilter.getHashIterations();
-        
-        long hash64 = MurmurHash3.hash64(data.getBytes());
-        int hash1 = (int) hash64;
-        int hash2 = (int) (hash64 >>> 32);
-        
-        List<Long> indices = new ArrayList<>();
-        for (int i = 1; i <= hashIterations; i++) {
-            long combinedHash = hash1 + (long) i * hash2;
-            if (combinedHash < 0) {
-                combinedHash = ~combinedHash;
-            }
-            indices.add(combinedHash % size);
+        log.info("BloomFilterHandler.add() called with data={}", data);
+        try {
+            boolean result = cachePenetrationBloomFilter.add(data);
+            log.info("BloomFilterHandler.add() result={}, size={}, count={}", 
+                    result, cachePenetrationBloomFilter.getSize(), cachePenetrationBloomFilter.count());
+            boolean check = cachePenetrationBloomFilter.contains(data);
+            log.info("BloomFilterHandler.add() immediate contains check={}", check);
+            return result;
+        } catch (Exception e) {
+            log.error("BloomFilterHandler.add() exception", e);
+            throw e;
         }
-        
-        String lua = "for i=1,#ARGV do\n" +
-            "    redis.call('setbit', KEYS[1], ARGV[i], 1)\n" +
-            "end\n" +
-            "return 1";
-        
-        List<Object> keys = Collections.singletonList(filterKey);
-        Object[] args = indices.toArray();
-        
-        script.eval(RScript.Mode.READ_WRITE, lua, RScript.ReturnType.VALUE, keys, args);
-        return true;
     }
     
     public boolean contains(String data) {
